cab404507557f97d8c43043c754bf601be70b1ed
Coco29dev <10059@holbertonstudents.com>
2024-10-21 11:55:44 +0000

    1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,13 @@
 
 int main(void)
 {
-printf("Size of a char: %zu byte(s)\n", sizeof(char));
-printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of a long: %zu byte(s)\n", sizeof(long));
-printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
-printf("Size of a float: %zu byte(s)\n", sizeof(float));
+int a;
+char b;
+float c;
+printf("Size of a char: %ld byte(s)\n", sizeof(b));
+printf("Size of an int: %ld byte(s)\n", sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(c));
 return (0);
 }
