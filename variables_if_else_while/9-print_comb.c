e02e1f2e9016ae3974c6e23dcd7384035aadba0f
Coco29dev <10059@holbertonstudents.com>
2024-10-22 11:26:45 +0000

    2

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - so cool
+*Return: 0 cool
+*/
+
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+putchar(x + '0');
+if (x < 9)
+putchar(',');
+putchar(' ');
+putchar('\n');
+return (0);
+}
