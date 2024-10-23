218dfe3b645ff1d7c609dc128441f30401223525
Coco29dev <10059@holbertonstudents.com>
2024-10-23 17:04:00 +0000

    3

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,8 @@
 int print_last_digit(int n)
 {
 int last_digit = n % 10;
-if (last_digit < 10)
-{
-last_digit *= -last_digit;
-}
+if (last_digit < 10)â
+last_digit = -last_digit;
 _putchar(last_digit + '0');
 return (last_digit);
 }
