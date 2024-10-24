ae271e73d540a84575b6cd64aa6a8db6926a6eb1
Coco29dev <10059@holbertonstudents.com>
2024-10-24 18:05:05 +0000

    3

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day from 00:00 to 23:59.
+ *
+ * Description: This function displays every minute of the day,
+ * starting at 00:00 and ending at 23:59, in the format HH:MM.
+ */
+
+void jack_bauer(void)
+{
+int heure = 0;
+int minute = 0;
+for (heure = 0; heure < 24; heure++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+printf("%02d:%02d\n", heure, minute);
+}
+}
+}
