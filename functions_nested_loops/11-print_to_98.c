07e00ccaa241a57d4101f25b87de100ccddc2192
Coco29dev <10059@holbertonstudents.com>
2024-10-25 12:01:04 +0000

    3

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,28 +6,15 @@
  */
 void print_to_98(int n)
 {
-if (n <= 98)
+while (n != 98)
 {
-for (; n < 98; n++)
+if (n < 98)
 {
-_putchar(n + '0');
-if (n < 97)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
+printf("%d, ", n++);
 }
 else
 {
-for (; n > 98; n--)
-{
-_putchar(n + '0');
-if (n > 99)
-{
-_putchar (',');
-_putchar(' ');
-}
+printf("%d, ", n--);
 }
 }
 _putchar('9');
