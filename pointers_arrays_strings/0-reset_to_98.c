66d127c1f348b55b7619dc67cb2d80453e8bab0c
Coco29dev <10059@holbertonstudents.com>
2024-10-29 08:44:31 +0000

    2

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * reset_to_98 - Updates the value pointed to by n to 98.
+ * @n: A pointer to an integer that will be updated.
+ *
+ * Description: This function takes a pointer to an integer
+ * and updates the value it points to 98. This is useful for
+ * resetting values to a specific default.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 402;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Project 2 for moire C with pointers !
