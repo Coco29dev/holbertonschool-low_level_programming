a473dcba02fbb67804439422c9b6413e9244bcdd
Coco29dev <10059@holbertonstudents.com>
2024-10-30 17:18:02 +0000

    3

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concatene deux chaines de caracteres
+ * @dest: pointeur vers la chaine de destination qui sera modifiee
+ * @src: pointeur vers la chaine source qui sera ajoute a la fin de dest
+ *
+ * Description: Cette fonction prend la chaibe src et l'ajoute
+ *a la fin de la chaine dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *original_dest = dest;
+	while (*dest != '\0')
+{
+	dest++;
+}
+	while (*src != '\0')
+{
+	*dest = *src;
+	dest ++;
+	src++;
+}
+	*dest = '\0';
+	return (original_dest);
+}
