d9acbe0e9c572b3b30fc9ce8182f0893d103fdf8
Coco29dev <10059@holbertonstudents.com>
2024-11-01 17:38:30 +0000

    3

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+char *string_toupper(char *)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - ('a' - 'A');
+}
+}
+return str;
+}
