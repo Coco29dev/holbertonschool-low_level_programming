93071d95c44c573a16a686a422d494677408a9a5
Coco29dev <10059@holbertonstudents.com>
2024-11-01 17:25:58 +0000

    3

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -3,8 +3,8 @@
 
 /**
  * reverse_array - Inverse le contenu d'un tableau d'entiers.
- * @arr: Pointeur vers le tableau d'entier a inverser.
- * @size: Taille du tableau d'entiers.
+ * @a: Pointeur vers le tableau d'entier a inverser.
+ * @n: Taille du tableau d'entiers.
  *
  * Description: Cette fonction prend un tableau d'entiers et inverse
  * son contenu
@@ -13,13 +13,13 @@
 void reverse_array(int *a, int n)
 {
 int s = 0;
-int e = size - 1;
+int e = n - 1;
 int t;
-while (s > e)
+while (s < e)
 {
-t = arr[s];
-arr[s] = arr[e];
-arr[e] = t;
+t = a[s];
+a[s] = a[e];
+a[e] = t;
 s++;
 e--;
 }
