60d63d9ec8e24d1edb88c59586c6d933915bad9d
Coco29dev <10059@holbertonstudents.com>
2024-11-04 14:38:12 +0000

    3p

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The string containing the accepted characters.
+ *
+ * Return: The number of bytes in the initial segment of s
+ *         which consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+char *a;
+while (*s)
+{
+for (a = accept; *a; a++)
+{
+if (*s == *a)
+{
+count++;
+break;
+}
+}
+if (!*a)
+{
+break;
+}
+s++;
+}
+return (count);
+}
