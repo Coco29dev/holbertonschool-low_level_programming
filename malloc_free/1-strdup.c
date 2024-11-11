0c3824c0ee43f6891d752ba12fd136322de1b229
Coco29dev <10059@holbertonstudents.com>
2024-11-11 09:36:55 +0000

    task1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,1 +1,21 @@
-#include
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+char *_strdup(char *str)
+{
+int i;
+char *cp_str;
+if (str == NULL)
+return (NULL);
+cp_str = malloc((strlen(str) + 1) * sizeof(char));
+if (cp_str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+cp_str[i] = str[i];
+}
+cp_str[strlen(str)] = '\0';
+return (cp_str);
+}
