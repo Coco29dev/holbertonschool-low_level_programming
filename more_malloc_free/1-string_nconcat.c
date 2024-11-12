fe090431b9f49ca09939fbc72a81678dffa52878
Coco29dev <10059@holbertonstudents.com>
2024-11-12 15:15:03 +0000

    try

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *cp_str;
-unsigned int i; j; len1 = 0; len2 = 0;
+unsigned int i, cp_i = 0, len1 = 0, len2 = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
@@ -12,10 +12,15 @@ s2 = "";
 while (s1[len1] != '\0')
 len1++;
 while (s2[len2] != '\0')
-len2++
-
-if (cp_str == NULL)
-return (NULL);
+len2++;
 if (n >= len2)
 n = len2;
-
+cp_str = malloc(sizeof(char) * (len1 + n + 1));
+if (cp_str == NULL)
+return (NULL);
+for (i = 0; s1[i]; i++)
+cp_str[cp_i++] = s1[i];
+for (i = 0; s2[i]; i++)
+cp_str[cp_i++] = s2[i];
+return (cp_str);
+}
