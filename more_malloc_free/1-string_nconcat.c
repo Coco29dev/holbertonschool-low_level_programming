a330b50006d9088a0e7ffb4768c13414e3b9f914
Coco29dev <10059@holbertonstudents.com>
2024-11-12 14:55:37 +0000

    test

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,21 @@
-#include
+#include <stdlib.h>
+#include "main.h"
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *cp_str;
+unsigned int i; j; len1 = 0; len2 = 0;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[len1] != '\0')
+len1++;
+while (s2[len2] != '\0')
+len2++
+
+if (cp_str == NULL)
+return (NULL);
+if (n >= len2)
+n = len2;
+
