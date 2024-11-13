1019d2ad354c42ebebdeddcac8a01bac22f7caab
Coco29dev <10059@holbertonstudents.com>
2024-11-13 09:31:00 +0000

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio>
+#include <stdio.h>
 struct dog
 {
 char *name;
 float age;
-char *owner
+char *owner;
 }
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 
 #endif
