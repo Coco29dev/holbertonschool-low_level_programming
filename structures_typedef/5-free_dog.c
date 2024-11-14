945235cd2152eb52244801f19a8d562e449f3925
Coco29dev <10059@holbertonstudents.com>
2024-11-14 14:59:45 +0000

    free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - Frees memory allocated for a dog.
+ * @d: The dog to free.
+ * Return: Nothing.
+ * Description: This function frees
+ * the memory allocated for the name and
+ * owner of the dog, and then frees the
+ * memory for the dog structure itself.
+ * It ensures that all dynamically
+ * allocated memory for the dog is properly
+ * deallocated to prevent memory leaks.
+ */
+
+void free_dog(dog_t *d)
+{
+if (d->name != NULL)
+{
+free(d->name);
+}
+if (d->owner != NULL)
+{
+free(d->owner);
+}
+return;
+}
