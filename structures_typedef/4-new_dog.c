53f345d2354a14ecb5b8c609bda3b72f25b6eca0
Coco29dev <10059@holbertonstudents.com>
2024-11-14 07:55:58 +0000

    newdoggy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
-#include <string.h>
+#include <stdlib.h>
 /**
- * new_dog - Creates a new dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- * Return: A pointer to the new dog_t structure, or NULL if the function fails
- *         (e.g., memory allocation failure).
- * Description: This function allocates memory for a new dog, initializes it
- * with the provided values for name, age, and owner, and returns a pointer
- * to the new dog_t structure. The function ensures that a copy of the name
- * and owner strings is stored, and handles memory allocation errors by
- * returning NULL and freeing any allocated memory.
- */
+* new_dog - create a new dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Return: pointer on new dog or NULL
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
+int nlen, olen, i;
+dog_t *doggy;
+nlen = olen = 0;
+while (name[nlen++])
+;
+while (owner[olen++])
+;
+doggy = malloc(sizeof(dog_t));
+if (doggy == NULL)
 return (NULL);
-new_dog->name = strdup(name);
-if (new_dog->name == NULL)
-{
-free(new_dog);
+doggy->name = malloc(nlen * sizeof(doggy->name));
+if (doggy == NULL)
 return (NULL);
+for (i = 0; i < nlen; i++)
+doggy->name[i] = name[i];
+doggy->age = age;
+doggy->owner = malloc(olen * sizeof(doggy->owner));
+if (doggy == NULL)
+return (NULL);
+for (i = 0; i < olen; i++)
+doggy->owner[i] = owner[i];
+return (doggy);
 }
-new_dog->owner = strdup(owner);
-if (new_dog->owner == NULL)
-{
-free(new_dog->name);
-free(new_dog);
-}
-new_dog->age = age;
-return (new_dog);
-}
-
