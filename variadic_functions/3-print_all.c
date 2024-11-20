3d5be9cbaef494aaacc03949686e287030d63f45
Coco29dev <10059@holbertonstudents.com>
2024-11-20 17:52:26 +0000

    3.4

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,42 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "variadic_functions.h"
 
-void print_all(const char * const format, ...)
+void print_char(va_list args)
 {
-va_list args;
-unsigned int i = 0;
-char *s;
-float f;
 char c;
-int fi = 1;
-va_start(args, format);
-while (format[i] != '\0')
-{
-if (!fi)
-printf(", ");
-fi = 0;
-if (format[i] == 'c')
-{
 c = va_arg(args, int);
 printf("%c", c);
 }
-else if (format[i] == 'i')
+void print_int(va_list args)
 {
-printf("%d", va_arg(args, int));
+int i;
+i = va_arg(args, int);
+printf("%d", i);
 }
-else if (format[i] == 'f')
+void print_float(va_list args)
 {
+float f;
 f = va_arg(args, double);
 printf("%f", f);
 }
-else if (format[i] == 's')
+void print_string(va_list args)
 {
-s = va_arg(args, char*);
-if (s == NULL)
+char *s;
+s = va_arg(args, char *);
+if (!s)
+{
 printf("(nil)");
-else
+return;
+}
 printf("%s", s);
 }
+void print_all(const char * const format, ...)
+{
+print_type types[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_string},
+{NULL, NULL}
+};
+va_list args;
+char *separator = "";
+int i = 0;
+int j = 0;
+va_start(args, format);
+while (format && format[i])
+{
+while (types[j].type)
+{
+if (*types[j].type == format[i])
+{
+printf("%s", separator);
+types[j].f(args);
+separator = ", ";
+}
+j++;
+}
+j = 0;
 i++;
 }
 printf("\n");
