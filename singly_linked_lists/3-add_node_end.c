7a30f15430209df301fcf6c6116459a5e0ab05ec
Coco29dev <10059@holbertonstudents.com>
2024-12-02 16:24:49 +0000

    code fonctionnel task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_node_end - add a new node at end
+ * @head: pointer to pointer
+ * @str: pointer to const string
+ * Return: the new node
+ *
+ */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -10,7 +17,15 @@ if (new_node == NULL)
 return (NULL);
 new_node->str = strdup(str);
 new_node->next = NULL;
+if (*head == NULL)
+{
 *head = new_node;
+return (new_node);
+}
+while (last->next != NULL)
+{
+last = last->next;
+}
 last->next = new_node;
 return (new_node);
 }
