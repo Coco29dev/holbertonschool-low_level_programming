5ea567cd40721ca53d7c5ce12ede8d85d4369437
Coco29dev <10059@holbertonstudents.com>
2024-12-02 16:32:06 +0000

    code fonctionnel task 4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,1 +1,22 @@
 #include "lists.h"
+
+/**
+ * free_list - free memory
+ * @head: pointer to pointer
+ * Return: nothing
+ *
+ */
+
+
+void free_list(list_t *head)
+{
+list_t *current = head;
+list_t *next_node;
+while (current != NULL)
+{
+next_node = current->next;
+free(current->str);
+free(current);
+current = next_node;
+}
+}
