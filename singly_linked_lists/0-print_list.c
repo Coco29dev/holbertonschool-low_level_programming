a3344c5c4cf2dbc226e52427ae9860b92a3964b9
Coco29dev <10059@holbertonstudents.com>
2024-12-02 11:58:20 +0000

    test task0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,9 +3,18 @@
 
 size_t print_list(const list_t *h)
 {
-
+int count = 0;
+const list_t *current = h;
+while (current != NULL)
+{
+current =  current->next;
+count++;
 if (str == NULL)
+{
 printf("[0] (nil)");
-
-return ();
+if (str != NULL)
+{
+printf("[len]", str);
+}
+return (count);
 }
