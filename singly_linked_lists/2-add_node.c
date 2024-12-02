4aed058afc49b7cb2523b01a57e396c8fe3a3fcf
Coco29dev <10059@holbertonstudents.com>
2024-12-02 14:49:45 +0000

    test n1  task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,12 +2,20 @@
 
 /**
  * add_node - add a new node
- * @:
- * Return:
+ * @head: pointer to pointer
+ * @str: pointer to const string
+ * Return: the new node
  *
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-
+list_t *new_node;
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+new_node->str = strdup(str);
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
