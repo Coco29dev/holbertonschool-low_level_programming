a86aa2f80d7eb6329dbf7c7111aedd2afaf649fd
Coco29dev <10059@holbertonstudents.com>
2024-12-02 16:41:23 +0000

    code fontionnel task avance

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,7 +1,12 @@
 #include "lists.h"
 
+/**
+ * function_before_main - print a string before main execute
+ * Return: nothing
+ */
 
 void function_before_main(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
