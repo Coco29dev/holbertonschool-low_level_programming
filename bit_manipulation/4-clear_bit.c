888d86fbfac78943a30d0937bcd4ca1a9c855f98
Coco29dev <10059@holbertonstudents.com>
2024-12-04 17:47:42 +0000

    code fonctionnel pour checker task 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 unsigned long int mask = 0;
 if (index > 63)
 return (-1);
-mask = ~1 << index;
+mask = ~(1 << index);
 *n = *n & mask;
 return (1);
 }
