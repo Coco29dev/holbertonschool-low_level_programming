a2a7092fa06bb75a2c525142c03864f9826455f1
Coco29dev <10059@holbertonstudents.com>
2024-12-04 14:12:55 +0000

    premire version du code, non fonctionnel

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+ * set_bit - returns the value of a bit given index
+ * @n: pointer integrer
+ * @index: index
+ * Return: value of bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+int mask = 0;
+if (index > 63)
+return (-1);
+mask = 1 << index;
+n = n | mask;
+return (1);
+}
