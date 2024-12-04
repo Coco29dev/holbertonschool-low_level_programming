827d4506bfdf755d382a30ccf274a579d3f2952e
Coco29dev <10059@holbertonstudents.com>
2024-12-04 13:29:19 +0000

    code fonctionnel avec la recursion task1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,1 +1,31 @@
 #include "main.h"
+
+/**
+ * print_binary_helper - print number binary help recursion function
+ * @n: number of binary
+ * Return: nothing
+ */
+
+void print_binary_helper(unsigned long int n)
+{
+if (n == 0)
+return;
+print_binary_helper(n >> 1);
+_putchar((n & 1) ? '1' : '0');
+}
+
+/**
+ * print_binary - print number binary
+ * @n: number of binary
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+print_binary_helper(n);
+}
