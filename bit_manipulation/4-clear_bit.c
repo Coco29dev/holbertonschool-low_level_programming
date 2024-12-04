c637c4906aa743a05e933b00fff1a5916fdfa292
Coco29dev <10059@holbertonstudents.com>
2024-12-04 17:44:05 +0000

    code fonctionnel task 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: pointer integrer
+ * @index: index
+ * Return: value of bit
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int mask = 0;
+if (index > 63)
+return (-1);
+mask = ~1 << index;
+*n = *n & mask;
+return (1);
+}
