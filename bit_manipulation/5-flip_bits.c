9855db00088e36bd384f3d327ab9ababdd250420
Coco29dev <10059@holbertonstudents.com>
2024-12-04 17:54:06 +0000

    code fonctionnel task 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you need to flip
+ * @n: first integrer
+ * @m: second integrer
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int flip = n ^ m;
+unsigned long int count = 0;
+while (flip)
+{
+count += flip & 1;
+flip >>= 1;
+}
+return (count);
+}
