b41d48d0503316a16020ba86da6f057dd3a01454
Coco29dev <10059@holbertonstudents.com>
2024-12-04 13:58:35 +0000

    code fonctionnel task2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,1 +1,16 @@
 #include "main.h"
+
+/**
+ * get_bit - returns the value of a bit given index
+ * @n: integrer
+ * @index: index
+ * Return: value of bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+n = n >> index;
+return (n & 1);
+}
