30cb262f0a4c34b45750ef96e8b3843e1ee6658c
Coco29dev <10059@holbertonstudents.com>
2024-12-04 10:17:34 +0000

    code fonctionnel task0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+ * binary_to_uint - conversion
+ * @b: chaine de caractere
+ * Return: le resultat de la conversion
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+int i = 0;
+unsigned int resultat = 0;
+if (b == NULL)
+return (0);
+while (b[i] != '\0')
+{
+if (b[i] != '0' && b[i] != '1')
+{
+return (0);
+}
+resultat = resultat * 2;
+if (b[i] == '1')
+{
+resultat += 1;
+}
+i++;
+}
+return (resultat);
+}
